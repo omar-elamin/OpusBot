9486d005981570a2a1fd386212ff78cbcaacfa87
Elmon11 <elmon11bussiness@gmail.com>
2021-03-14 17:30:47 +0100

    Update developer_tools.py

--- cogs/commands/developer_tools.py
+++ cogs/commands/developer_tools.py
@@ -6,14 +6,15 @@ from discord.ext import commands
 import utils.checks as checks
 
 
-# A set of tools for the bot developer to use when testing the bot
 class DevTools(commands.Cog, name='Developer Tools'):
     def __init__(self, bot):
+        """ A set of tools for the bot developer to use when testing the bot. """
         self.bot = bot
 
     @checks.is_developer()  # Checks that the person sending the command is a bot developer.
     @commands.command(aliases=['eval'])
     async def debug(self, ctx, code):
+        
         """Executes a line of code for debugging."""
         em = discord.Embed()
         em.title = 'Result'
@@ -31,7 +32,7 @@ class DevTools(commands.Cog, name='Developer Tools'):
     @checks.is_developer()  # Checks that the person sending the command is a bot developer.
     @commands.command()
     async def shutdown(self, ctx):
-        """Shuts down the bot."""
+        """ Shuts down the bot. """
         em = discord.Embed()
         em.title = 'Shutdown'
         em.description = 'Bot offline.'
